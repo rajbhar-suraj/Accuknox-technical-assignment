08cd9ae25f9c514924a2094db084a0a933b4294f
Suraj Rajbhar <suraj.rajbhar.tech@gmail.com>
2025-09-07 16:32:08 +0530

    resolution

--- src/store/useLocalStorage.js
+++ src/store/useLocalStorage.js
@@ -7,17 +7,28 @@ const useLocalStore = create(persist((set, get) => ({
     categories: [
         {
             id: 1,
-            name: "CSMP EXECUTIVE DASHBOARD",
+            name: "CSMP Executive Dashboard",
             widgets: [
-                { id: 1, widgetName: "some random1", widgetText: "some random text1" },
-                { id: 2, widgetName: "some random2", widgetText: "some random text2" },
+                { id: 1, widgetName: "Cloud Accounts", widgetText: "Random text 1" },
+                { id: 2, widgetName: "Cloud Account Risk Assesment", widgetText: "Random text 2" },
             ],
         },
         {
             id: 2,
-            name: "CWPP DASHBOARD",
+            name: "CWPP Dashboard",
             widgets: [
-                { id: 3, widgetName: "some random3", widgetText: "some random text3" },
+                { id: 1, widgetName: "Top 5 Namespace Specific Alerts", widgetText: "some random text 3" },
+                { id: 2, widgetName: "Work Alerts", widgetText: "some random text 4" },
+
+            ],
+        },
+        {
+            id: 3,
+            name: "Registry Scan",
+            widgets: [
+                { id: 1, widgetName: "Image Risk Assessment", widgetText: "some random text 5" },
+                { id: 2, widgetName: "Image Security Issues", widgetText: "some random text 6" },
+
             ],
         },
     ],
@@ -74,8 +85,8 @@ const useLocalStore = create(persist((set, get) => ({
     },
 }),
     {
-        name: "dashboard-storage", 
-        getStorage: () => localStorage, 
+        name: "dashboard-storage",
+        getStorage: () => localStorage,
     }
 
 ))
